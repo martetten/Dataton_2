71f015bbe736bdfef1ebd138fd0659db8265a648
ynastt <yarovikovaanastasia1@gmail.com>
2025-05-06 02:46:35 +0300

    Сохранение введенного текста

--- src/app/app.py
+++ src/app/app.py
@@ -15,15 +15,16 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def home():
     prediction = None
+    text = ""
     if request.method == 'POST':
-        text = request.form['text']
+        text = request.form.get('text', '')
         # Векторизация текста
         # X = vectorizer.transform([text])
         # Предсказание
         # pred = model.predict(X)[0]
         # prediction = categories[pred]
     
-    return render_template('index.html', prediction=prediction)
+    return render_template('index.html', prediction=prediction, text=text)
 
 if __name__ == '__main__':
     app.run(debug=True)
